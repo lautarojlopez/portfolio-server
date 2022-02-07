fa19781c29d009b0fa65db5eb71bf9166a2941ce
Lautaro López <lopezlautarojoaquin1@gmail.com>
2022-02-07 17:28:26 -0300

    Primera version finalizada

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors())
+app.post('/', (req, res) => {
+
+	const transporter = nodemailer.createTransport({
+		host: "smtp.googlemail.com",
+		port: 587,
+		secure: false,
+		auth: {
+			user: process.env.EMAIL,
+			pass: process.env.PASSWORD
+		}
+	})
+
+	const {nombre, email, asunto, mensaje} = req.body
+	const options = {
+		from: email,
+		to: process.env.MI_EMAIL,
+		subject: asunto,
+		text: `${mensaje} - ${nombre}`
+	}
+
+	transporter.sendMail(options, (error, info) => {
+		if(error){
+			console.log(error);
+			res.json({
+				title: "Error al enviar el mensaje",
+				msg: "Intentalo nuevamente"
+			})
+		}
+		else{
+			res.status(200).json({
+				title: "Mensaje enviado",
+				msg: "¡Gracias por ponerte en contacto conmigo!"
+			})
+		}
+	})
+
+})
+
+app.listen(4000)
